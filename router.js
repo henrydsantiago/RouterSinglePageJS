a64bf3e2e16a7b7f4490f16d9e7c24691f94addc
henrydsantiago <henryuny@gmail.com>
2020-03-01 04:32:39 -0300

    fin del curso

---
+++ router.js
@@ -0,0 +1,33 @@
+class Router{
+    constructor (routes){
+        this.routes = routes;
+        this._loadInitialRoute();
+    }
+
+    loadRoute(...urlSegs){
+        const matchedRoute = this._matchUrlToRoute(urlSegs);
+        const url = `/${urlSegs.join('/')}`;
+        history.pushState({}, 'this works', url);
+        const routerOutelm = document.querySelectorAll('[data-router]')[0];
+        routerOutelm.innerHTML = matchedRoute.template;
+    }
+
+    _loadInitialRoute(){
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit.slice(1) : '';
+        this.loadRoute(...pathSegs);
+    }
+
+    _matchUrlToRoute(urlSegs){
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1)
+            if (routePathSegs.length !== urlSegs.length){
+                return false;
+            }
+            return routePathSegs
+                .every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        });
+
+        return matchedRoute;
+    }
+}
